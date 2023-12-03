1404e2d16d449a4f3f136fc9f02b2192af20326e
buithuyngoc <buithuyngoc1@gmail.com>
2023-12-03 10:41:27 +0700

    feat: add some sql ex

--- database/BTTH01_CSE485.sql
+++ database/BTTH01_CSE485.sql
@@ -43,14 +43,14 @@ INSERT INTO tacgia (ma_tgia, ten_tgia) VALUES (8, "Tâm tình");
 
 -- Thêm dữ liệu vào The loai
 
-INSERT INTO theloai VALUE (1, "Nhạc trẻ");
-INSERT INTO theloai VALUE (2, "Nhạc trữ tình");
-INSERT INTO theloai VALUE (3, "Nhạc cách mạng");
-INSERT INTO theloai VALUE (4, "Nhạc thiếu nhi");
-INSERT INTO theloai VALUE (5, "Nhạc quê hương");
-INSERT INTO theloai VALUE (6, "POP");
-INSERT INTO theloai VALUE (7, "Rock");
-INSERT INTO theloai VALUE (8, "R&B");
+INSERT INTO theloai(ma_tloai, ten_tloai) VALUE (1, "Nhạc trẻ");
+INSERT INTO theloai(ma_tloai, ten_tloai) VALUE (2, "Nhạc trữ tình");
+INSERT INTO theloai(ma_tloai, ten_tloai) VALUE (3, "Nhạc cách mạng");
+INSERT INTO theloai(ma_tloai, ten_tloai) VALUE (4, "Nhạc thiếu nhi");
+INSERT INTO theloai(ma_tloai, ten_tloai) VALUE (5, "Nhạc quê hương");
+INSERT INTO theloai(ma_tloai, ten_tloai) VALUE (6, "POP");
+INSERT INTO theloai(ma_tloai, ten_tloai) VALUE (7, "Rock");
+INSERT INTO theloai(ma_tloai, ten_tloai) VALUE (8, "R&B");
 
 -- Thêm dữ liệu vào Baiviet
 
